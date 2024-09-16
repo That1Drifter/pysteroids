6a923c91bdb2a5617c6aa88c5e3f10e7137d93a8
that1drifter <americanemt@gmail.com>
2024-09-16 12:52:59 -0500

    60 fps

--- main.py
+++ main.py
@@ -2,10 +2,9 @@ import pygame
 from constants import *
 
 def main():
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         for event in pygame.event.get():
@@ -13,6 +12,8 @@ def main():
                 return
         screen.fill((0, 0, 0))
         pygame.display.flip()
+        clock.tick(60)
+        dt = clock.get_time() / 1000
 
 
 if __name__ == "__main__":
