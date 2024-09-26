db6fa2186cc999def2e28c490837bad25dd05f8b
that1drifter <americanemt@gmail.com>
2024-09-25 21:10:12 -0500

    player movement

--- main.py
+++ main.py
@@ -4,22 +4,29 @@ from player import Player
 
 def main():
     pygame.init()
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    Player.containers = (updateable, drawable)
     player = Player(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
-    
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         
-        player.update(dt)
+        for sprite in updateable:
+            sprite.update(dt)
+
         screen.fill(("black"))
-        player.draw(screen)
+
+        for sprite in drawable:
+            sprite.draw(screen)
+        
         pygame.display.flip()
         dt = clock.tick(60) / 1000
-
 
 if __name__ == "__main__":
     main()
